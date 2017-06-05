65bacb600884d90be2c1cf9f9aef7c963af59441
Sheharyar Naseer <hello@sheharyar.me>
2017-06-04 23:10:12 -0400

    Test for BetterParams plug with a minimal router

--- test/better_params_test.exs
+++ test/better_params_test.exs
@@ -1,5 +1,8 @@
 defmodule BetterParams.Tests do
   use ExUnit.Case
+  use Plug.Test
+
+  alias BetterParams.Tests.Meta.Router
 
 
   test "#symbolize_merge symbolizes a map and merges it with itself" do
@@ -15,6 +18,27 @@ defmodule BetterParams.Tests do
     m_after  = Map.merge(m_before, %{a: 1, b: %{c: 2, d: "3"}})
 
     assert BetterParams.symbolize_merge(m_before) == m_after
+  end
+
+
+
+  @opts Router.init([])
+  test "params map has both atom and string keys" do
+    params =
+      :get
+      |> conn("/test/1/2/3")        # New Connection
+      |> Router.call(@opts)         # Invoke Router
+      |> Map.get(:params)           # Fetch Params
+
+
+    # Assert param values
+    assert params[:a]  == "1"
+    assert params[:b]  == "2"
+    assert params[:c]  == "3"
+
+    assert params["a"] == "1"
+    assert params["b"] == "2"
+    assert params["c"] == "3"
   end
 
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,18 @@
+
+# Minimal Router to test our Plug against
+defmodule BetterParams.Tests.Meta.Router do
+  use Plug.Router
+
+  plug :match
+  plug :dispatch
+  plug BetterParams
+
+  get "/test/:a/:b/:c" do
+    send_resp(conn, 200, "ok")
+  end
+end
+
+
+# Run our tests
 ExUnit.start()
+
