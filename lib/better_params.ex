e3dd0aa720c06a1e7c1047d3ec38eb44cf93dad9
Sheharyar Naseer <hello@sheharyar.me>
2017-06-04 23:11:31 -0400

    Implemented plug logic to pass tests

--- lib/better_params.ex
+++ lib/better_params.ex
@@ -1,15 +1,16 @@
 defmodule BetterParams do
   import Plug.Conn
 
-  def init(opts), do: opts
+  def init(_), do: :ok
 
-  def call(conn, _) do
-    conn
+  def call(%{params: params} = conn, :ok) do
+    %{ conn | params: symbolize_merge(params) }
   end
 
 
   def symbolize_merge(map) when is_map(map) do
     map
+    |> Map.delete(:__struct__)
     |> ExUtils.Map.symbolize_keys(deep: true)
     |> Map.merge(map)
   end
