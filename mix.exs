d3ad465a6052b6389d3f13b39a6b232fa84678aa
Sheharyar Naseer <hello@sheharyar.me>
2017-06-14 03:44:29 -0400

    Bump version to v0.2.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule BetterParams.Mixfile do
 
   @app     :better_params
   @name    "BetterParams"
-  @version "0.1.0"
+  @version "0.2.0"
   @github  "https://github.com/sheharyarn/#{@app}"
 
 
